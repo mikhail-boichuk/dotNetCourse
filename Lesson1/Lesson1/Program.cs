8120f27775f65b1af41b67cac15a09a0ac3dd0bd
Boichuk Mikhail <mikhail.boichuk@hpe.com>
2018-04-02 14:01:39 +0300

    finished Lesson 1

--- Lesson1/Lesson1/Program.cs
+++ Lesson1/Lesson1/Program.cs
@@ -10,7 +10,48 @@ namespace Lesson1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String cont;
+
+            while (true)
+            {
+                Console.WriteLine("Please enter preson age:");
+                int age = Convert.ToInt32(Console.ReadLine());
+
+                if (age <= 0 || age > 100)
+                {
+                    Console.WriteLine("invalid");
+                }
+                else if (age >= 1 && age < 18)
+                {
+                    Console.WriteLine("kid");
+                }
+                else if (age >= 18 && age < 60)
+                {
+                    Console.WriteLine("adult");
+                }
+                else if (age >= 60 && age <= 100)
+                {
+                    Console.WriteLine("old man");
+                }
+                // we exclude one codition check, but in case of invalid input, programm will go thru all conditions anyway
+                /* else
+                {
+                    Console.WriteLine("invalid");
+                }
+                */
+
+                Console.WriteLine("wanna input again(y/n)?");
+                cont = Convert.ToString(Console.ReadLine());
+
+                if (cont == "y")
+                {
+                    continue;
+                } else
+                {
+                    break;
+                }
+            }
+
         }
     }
 }
