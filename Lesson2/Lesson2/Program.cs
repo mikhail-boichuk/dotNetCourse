6aa568f9169a9b70db4887581a7931613901f99e
Boichuk Mikhail <mikhail.boichuk@hpe.com>
2018-04-04 09:13:55 +0300

    fixed List<> types

--- Lesson2/Lesson2/Program.cs
+++ Lesson2/Lesson2/Program.cs
@@ -13,39 +13,39 @@ namespace Lesson2
     class Program
     {
         //aquire attrations available today
-        public static List<int> GetAvailableAttr(Days day)
+        public static List<Attractions> GetAvailableAttr(Days day)
         {
-            List<int> avalaibleAttractions = new List<int>();
+            List<Attractions> avalaibleAttractions = new List<Attractions>();
             
             if (day == Days.Sunday)
             {
-                avalaibleAttractions.Add((int)Attractions.Pony);
+                avalaibleAttractions.Add(Attractions.Pony);
             }
             if (day == Days.Monday || day == Days.Wednesday || day == Days.Friday)
             {
-                avalaibleAttractions.Add((int)Attractions.Batman);
+                avalaibleAttractions.Add(Attractions.Batman);
             }
             if (day == Days.Tuesday || day == Days.Wednesday || day == Days.Thursday)
             {
-                avalaibleAttractions.Add((int)Attractions.Swan);
+                avalaibleAttractions.Add(Attractions.Swan);
             }
 
             return avalaibleAttractions;
         }
 
         //match available attractions with kid's parameters
-        public static List<int> MatchAttractions(int height, Gender gender, Days day)
+        public static List<Attractions> MatchAttractions(int height, Gender gender, Days day)
         {
-            List<int> matchedAttractions = new List<int>();
-            foreach (int attr in GetAvailableAttr(day))
+            List<Attractions> matchedAttractions = new List<Attractions>();
+            foreach (Attractions attr in GetAvailableAttr(day))
             {
-                if (attr == (int)Attractions.Batman && gender == Gender.male && height > 150)
+                if (attr == Attractions.Batman && gender == Gender.male && height > 150)
                 {
                     matchedAttractions.Add(attr);
-                } else if (attr == (int)Attractions.Swan && ((gender == Gender.male && height < 140) || (gender == Gender.female && height > 120 && height < 140)))
+                } else if (attr == Attractions.Swan && ((gender == Gender.male && height < 140) || (gender == Gender.female && height > 120 && height < 140)))
                 {
                     matchedAttractions.Add(attr);
-                } else if (attr == (int)Attractions.Pony)
+                } else if (attr == Attractions.Pony)
                 {
                     matchedAttractions.Add(attr);
                 }
@@ -54,14 +54,14 @@ namespace Lesson2
         }
 
         //print list of matched attractions and kids name
-        public static void PrintMatchedAttrations (String name, List<int> matchedAttractions)
+        public static void PrintMatchedAttrations (String name, List<Attractions> matchedAttractions)
         {
             Console.WriteLine("{0} can go to:", name);
             if (matchedAttractions.Count > 0)
             {
-                foreach (int attr in matchedAttractions)
+                foreach (Attractions attr in matchedAttractions)
                 {
-                    Console.WriteLine((Attractions)attr);
+                    Console.WriteLine(attr);
                 }
             } else
             {
