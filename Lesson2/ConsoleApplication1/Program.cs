26152ca4140807d1f715f01a4f07cfa6a82c7e9a
Boichuk Mikhail <mikhail.boichuk@hpe.com>
2018-04-04 15:06:40 +0300

    ooops, now added palindrome task

---
+++ Lesson2/ConsoleApplication1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public static bool isPalindrome(string str)
+        {
+            if (str.Length >= 2)
+            {
+                // if first and last chars are identical, trim string and pass it to isPalindrome()
+                if (str[0] == str[str.Length-1])
+                {
+                    return isPalindrome(str.Substring(1,str.Length - 2));
+                }
+            } else
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            // Input string
+            Console.WriteLine("Please enter string:");
+            string str = Convert.ToString(Console.ReadLine());
+
+            // check if string is a palindrome
+            Console.WriteLine("Is it a palindrome? - {0}", isPalindrome(str));
+            Console.ReadLine();
+        }
+    }
+}
